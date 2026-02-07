b4208c9a32059eccee7a88ba3157940156c13eab
Deniz Hınçal <dhinc@uic.edu>
2026-02-07 06:30:32 -0600

    Change How To Play Design

--- chi-noir/app/how-to-play/page.tsx
+++ chi-noir/app/how-to-play/page.tsx
@@ -1,19 +1,45 @@
+import { FILE } from "dns/promises";
+import Link from "next/link";
+
 export default function HowToPlay() {
   return (
-    <div className="flex min-h-screen items-center justify-center dark:bg-black">
-      <div className="relative w-screen h-screen">
-        <div className="text-lg text-white bg-linear-to-b from-transparent from-20% to-30% to-black absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-primary h-full w-full flex items-center justify-center flex-col">
-          <h1 className="text-4xl font-bold text-white mb-4">How To Play</h1>
-          <p className="">
-            In Chi-Noir, you play as a detective trying to solve a murder case.
-            You will gather clues, some of which may be misleading, or the
-            ultimate truth you are looking for.
-          </p>
+    <div className="relative max-w-2xl w-full bg-[#e8dcc2] text-zinc-900 p-10 rounded-sm shadow-[0_20px_60px_rgba(0,0,0,0.7)] mx-auto mt-20">
+      <div className="absolute -top-6 left-8 bg-[#d6c7a5] px-6 py-2 text-sm tracking-widest shadow-md">
+        CASE FILE #001
+      </div>
+      <div className="absolute top-8 right-8 rotate-12 border-2 border-red-800 text-red-800 px-4 py-1 text-xs tracking-widest">
+        CONFIDENTIAL
+      </div>
+      <h1 className="text-3xl tracking-widest mb-6">CASE BRIEFING</h1>
+
+      <div className="space-y-6 text-sm leading-relaxed">
+        <div>
+          <h2 className="font-bold tracking-wider">CLUES ARE IMPORTANT</h2>
           <p>
-            While looking for clues, you may not have enough time to inspect
-            every single one, choose wisely.
+            Listen carefully to every testimony. Truth often hides between
+            words.
           </p>
         </div>
+
+        <div>
+          <h2 className="font-bold tracking-wider">DECISIONS</h2>
+          <p>
+            You will not have much time, choose where you want to focus wisely.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="font-bold tracking-wider">CONSEQUENCES</h2>
+          <p>Accuse the wrong suspect… and the killer walks free.</p>
+        </div>
+      </div>
+      <div className="mt-10 text-center">
+        <Link
+          href="/"
+          className="px-8 py-3 bg-zinc-900 text-white tracking-widest hover:bg-black transition cursor-pointer rounded-md shadow-[0_0_8px_rgba(0,0,0,0.25)] hover:shadow-[0_0_18px_rgba(0,0,0,0.7)]"
+        >
+          I AM READY!
+        </Link>
       </div>
     </div>
   );
