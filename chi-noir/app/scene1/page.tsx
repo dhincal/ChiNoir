e077d9863791058a1c6b6e24e739b95e5bd35ce2
Deniz Hınçal <dhinc@uic.edu>
2026-02-06 20:28:21 -0600

    Cleanup/HeroLanding

---
+++ chi-noir/app/scene1/page.tsx
@@ -0,0 +1,8 @@
+export default function Scene1() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
+      <h1 className="text-4xl font-bold text-white mb-4"></h1>
+      <p className="text-lg text-white"></p>
+    </div>
+  );
+}
