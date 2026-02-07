dd9090f4a9bb4e76654a0799c9dccdcc39185787
Deniz Hınçal <dhinc@uic.edu>
2026-02-06 22:41:37 -0600

    Action Buttons Functional

--- chi-noir/app/scene1/page.tsx
+++ chi-noir/app/scene1/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
 import MessageBox from "@/components/MessageBox";
 import LaSelleStationImg from "@/public/Scene1/LaSelleStation.png";
+import { useEffect, useState } from "react";
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default function Scene1() {
+  const [checkedBody, setCheckedBody] = useState(false);
+  const [checkedLogs, setCheckedLogs] = useState(false);
+  const [checkedRailway, setCheckedRailway] = useState(false);
+
+  let actionEvents: Function[] = [
+    () => {
+      setCheckedBody(true);
+      delay(500).then(() =>
+        document
+          .getElementById("BodyCheck")
+          ?.scrollIntoView({ behavior: "smooth" }),
+      );
+    },
+    () => {
+      setCheckedLogs(true);
+    },
+    () => {
+      setCheckedRailway(true);
+    },
+  ];
+
   let sceneMessages: string[] = [
     "Welcome to Laselle Stop, Detective. This is the scene of the crime.",
     "We have found out that the victim was a local bouncer named Jackob who works at Rex Lounge.",
@@ -10,17 +36,41 @@ export default function Scene1() {
     "Let's start by examining the crime scene and collecting any clues we can find, let me know where would you like to investigate first.",
   ];
 
+  useEffect(() => {
+    if (checkedBody && checkedLogs && checkedRailway) {
+      // All actions completed, proceed to next scene or show a message
+      alert(
+        "Great work, Detective! You've gathered all the evidence. Let's move on to the next scene.",
+      );
+      // Here you can navigate to the next scene or reset the state for another investigation
+    }
+  }, [checkedBody, checkedLogs, checkedRailway]);
+
   return (
-    <div
-      className={`flex min-h-screen items-center justify-center font-sans sceneViginette`}
-      style={{
-        backgroundImage: `url(${LaSelleStationImg.src})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <MessageBox messages={sceneMessages} />
+    <div>
+      <div
+        className={`flex min-h-screen items-center justify-center font-sans sceneViginette`}
+        style={{
+          backgroundImage: `url(${LaSelleStationImg.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <MessageBox messages={sceneMessages} actionHrefs={actionEvents} />
+      </div>
+      <div
+        id="BodyCheck"
+        className={`flex min-h-screen items-center justify-center font-sans sceneViginette relative ${checkedBody ? "" : "hidden"}`}
+        style={{
+          backgroundImage: `url(${LaSelleStationImg.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <MessageBox messages={sceneMessages} actionHrefs={actionEvents} />
+      </div>
     </div>
   );
 }
--- chi-noir/components/MessageBox.tsx
+++ chi-noir/components/MessageBox.tsx
@@ -3,23 +3,34 @@
 import Image from "next/image";
 import Cop from "@/public/Komser.png";
 import { useEffect, useState } from "react";
+import ActionButton from "./ActionButton";
 
 export default function MessageBox({
   messages,
+  actionHrefs,
 }: Readonly<{
   messages: string[];
+  actionHrefs?: Function[];
 }>) {
   const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [showActionButtons, setShowActionButtons] = useState(false);
 
   const handleClick = () => {
     if (currentMessageIndex < messages.length - 1) {
       setCurrentMessageIndex(currentMessageIndex + 1);
     }
   };
+
+  useEffect(() => {
+    if (currentMessageIndex === messages.length - 1) {
+      setShowActionButtons(true);
+    }
+  }, [currentMessageIndex, messages.length]);
+
   return (
     <div
       style={{ zIndex: 5 }}
-      className="absolute bottom-1/4 left-1/2 transform -translate-x-1/2  text-white p-6 rounded-lg min-w-2xl max-w-5xl cursor-pointer"
+      className="absolute bottom-1/6 left-1/2 transform -translate-x-1/2  text-white p-6 rounded-lg min-w-2xl max-w-5xl cursor-pointer"
       onClick={handleClick}
     >
       <div className="relative z-20">
@@ -34,6 +45,22 @@ export default function MessageBox({
             {messages[currentMessageIndex]}
           </p>
           <p className="text-sm text-gray-400 mt-2">Click to continue...</p>
+        </div>
+        <div
+          className={`transition-all flex flex-row justify-between gap-x-5 mt-5 duration-300 ${showActionButtons ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+        >
+          <ActionButton
+            label="Check Body"
+            onClick={() => (actionHrefs ? actionHrefs[0]() : null)}
+          />
+          <ActionButton
+            label="Check Logs"
+            onClick={() => (actionHrefs ? actionHrefs[1]() : null)}
+          />
+          <ActionButton
+            label="Check Railway"
+            onClick={() => (actionHrefs ? actionHrefs[2]() : null)}
+          />
         </div>
       </div>
     </div>
